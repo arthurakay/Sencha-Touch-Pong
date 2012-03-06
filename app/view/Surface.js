62cdf5bfae27dc87cb2cb9e0667e47edbe2b2b76
Arthur Kay <arthurakay@art.gateway.2wire.net>
2012-03-06 16:57:18 -0600

    Fixed issue #11  - only allow veritical dragging

--- app/view/Surface.js
+++ app/view/Surface.js
@@ -1,129 +1,131 @@
 Ext.define('Pong.view.Surface', {
-    extend: 'Ext.Panel',
-    requires: [
+    extend   : 'Ext.Panel',
+    requires : [
         'Pong.view.Ball',
         'Pong.view.Paddle'
     ],
 
-    config: {
-        layout: {
-            type: 'fit'
+    config : {
+        layout : {
+            type : 'fit'
         },
-        items: [
+        items  : [
             {
-                xtype: 'toolbar',
-                docked: 'top',
-                height: 75,
-                title: 'Pong',
-                items: [
+                xtype  : 'toolbar',
+                docked : 'top',
+                height : 75,
+                title  : 'Pong',
+                items  : [
                     {
-                        xtype: 'button',
-                        itemId: 'playpause',
-                        ui: 'confirm',
-                        text: 'Play / Pause'
+                        xtype  : 'button',
+                        itemId : 'playpause',
+                        ui     : 'confirm',
+                        text   : 'Play / Pause'
                     },
                     {
-                        xtype: 'spacer'
+                        xtype : 'spacer'
                     },
                     {
-                        xtype: 'sliderfield',
-                        itemId: 'mysliderfield',
-                        width: 300,
-                        label: 'Difficulty',
-                        labelWidth: 100,
-                        value: [
+                        xtype      : 'sliderfield',
+                        itemId     : 'mysliderfield',
+                        width      : 300,
+                        label      : 'Difficulty',
+                        labelWidth : 100,
+                        value      : [
                             1
                         ],
-                        maxValue: 5,
-                        minValue: 1
+                        maxValue   : 5,
+                        minValue   : 1
                     }
                 ]
             },
             {
-                xtype: 'ball',
-                itemId: 'ball'
+                xtype  : 'ball',
+                itemId : 'ball'
             },
             {
-                xtype: 'paddle',
-                draggable: true,
-                itemId: 'player',
-                right: 100
+                xtype     : 'paddle',
+                draggable : {
+                    direction : 'vertical'
+                },
+                itemId    : 'player',
+                right     : 100
             },
             {
-                xtype: 'paddle',
-                itemId: 'cpu',
-                left: 100
+                xtype  : 'paddle',
+                itemId : 'cpu',
+                left   : 100
             },
             {
-                xtype: 'toolbar',
-                docked: 'top',
-                items: [
+                xtype  : 'toolbar',
+                docked : 'top',
+                items  : [
                     {
-                        xtype: 'component',
-                        cls: [
+                        xtype  : 'component',
+                        cls    : [
                             'score'
                         ],
-                        html: 'CPU: 0',
-                        itemId: 'scoreCPU'
+                        html   : 'CPU: 0',
+                        itemId : 'scoreCPU'
                     },
                     {
-                        xtype: 'spacer'
+                        xtype : 'spacer'
                     },
                     {
-                        xtype: 'component',
-                        cls: [
+                        xtype  : 'component',
+                        cls    : [
                             'score'
                         ],
-                        html: 'Player: 0',
-                        itemId: 'scorePlayer'
+                        html   : 'Player: 0',
+                        itemId : 'scorePlayer'
                     }
                 ]
             },
             {
-                xtype: 'container',
-                hidden: true,
-                itemId: 'Sound',
-                items: [
+                xtype  : 'container',
+                hidden : true,
+                itemId : 'Sound',
+                items  : [
                     {
-                        xtype: 'audio',
-                        hidden: true,
-                        itemId: 'collision',
-                        autoPause: false,
-                        autoResume: true,
-                        enableControls: false,
-                        muted: false,
-                        url: 'assets/sounds/border.wav'
+                        xtype          : 'audio',
+                        hidden         : true,
+                        itemId         : 'collision',
+                        autoPause      : false,
+                        autoResume     : true,
+                        enableControls : false,
+                        muted          : false,
+                        url            : 'assets/sounds/border.wav'
                     },
                     {
-                        xtype: 'audio',
-                        hidden: true,
-                        itemId: 'win',
-                        autoPause: false,
-                        autoResume: true,
-                        enableControls: false,
-                        muted: false,
-                        url: 'assets/sounds/cheer.wav'
+                        xtype          : 'audio',
+                        hidden         : true,
+                        itemId         : 'win',
+                        autoPause      : false,
+                        autoResume     : true,
+                        enableControls : false,
+                        muted          : false,
+                        url            : 'assets/sounds/cheer.wav'
                     },
                     {
-                        xtype: 'audio',
-                        hidden: true,
-                        itemId: 'lose',
-                        autoPause: false,
-                        autoResume: true,
-                        enableControls: false,
-                        muted: false,
-                        url: 'assets/sounds/boo.wav'
+                        xtype          : 'audio',
+                        hidden         : true,
+                        itemId         : 'lose',
+                        autoPause      : false,
+                        autoResume     : true,
+                        enableControls : false,
+                        muted          : false,
+                        url            : 'assets/sounds/boo.wav'
                     },
                     {
-                        xtype: 'audio',
-                        height: 22,
-                        hidden: true,
-                        itemId: 'pong',
-                        width: 22,
-                        autoPause: false,
-                        autoResume: true,
-                        muted: false,
-                        url: 'assets/sounds/pong.wav'
+                        xtype      : 'audio',
+                        height     : 22,
+                        hidden     : true,
+                        itemId     : 'pong',
+                        width      : 22,
+                        autoPause  : false,
+                        autoResume : true,
+                        muted      : false,
+                        url        : 'assets/sounds/pong.wav'
                     }
                 ]
             }
