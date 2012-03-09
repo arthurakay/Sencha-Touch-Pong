071f48c7fb3c852f6e88e8d45e517512af2f95bb
Arthur Kay <arthurakay@gmail.com>
2012-03-09 09:48:54 -0600

    Updated build process

--- _sencha-build-app.sh
+++ _sencha-build-app.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+rm -rf build/*
+
 sencha build -p pong.jsb3 -d ./build
 
-cp pong.css ./build/pong.css
-cp -R assets ./build/assets
+cp build.html ./build/index.html
+cp pong.css ./build
+cp app.js ./build
+
+mkdir ./build/assets
+cp assets/sencha-touch-2.0.0-gpl/resources/css/sencha-touch.css ./build/assets/
+cp assets/sencha-touch-2.0.0-gpl/sencha-touch-all.js ./build/assets/
+
+mkdir ./build/assets/sounds
+cp -R assets/sounds ./build/assets
+
+zip -r -X build/build.zip build
